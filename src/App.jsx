6d2576423f5972a9885c2df428160e689f7b690a
Daniel <skyrimdaniel11@gmail.com>
2023-03-21 14:27:07 -0300

    css

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,54 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import Toast from './components/Toast'
 
 function App() {
-  const [count, setCount] = useState(0)
 
+  const [password, setPassword] = useState('')
+const [render, setRender] = useState(false)
+const [message, setMessage] = useState(false)
+  function handlePasswordChecker() {
+    //empty verify
+     if (password === '') {
+      ShowToast('Cadê a senha? 🕵🏽‍♀️')
+
+     }
+     else if(password.length<8){
+      ShowToast('A senha precisa de no minímo 8 dígitos 🙁')
+     }
+
+
+
+
+
+
+  }
+
+  function ShowToast(message){
+    setMessage(message)
+   setRender(true)
+
+
+     setTimeout(() => {
+        setRender(false)
+      }, 3000)
+     
+  }
   return (
     <>
-    <h1>ddddd</h1>
       <div className="App">
+        <h1>Welcome to password checker</h1>
         <div className="container-search">
-          <input className="input-search" type="text" />
-          <button>Send</button>
+          <input
+            onChange={(e) => setPassword(e.target.value)}
+            className="input-search"
+            type="text"
+          />
+          <button onClick={handlePasswordChecker}>Send</button>
         </div>
+
+        {render ? <Toast className="toast-fadeout">{message}</Toast> : null}
       </div>
     </>
   )
